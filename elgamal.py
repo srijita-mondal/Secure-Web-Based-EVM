7380e5d74f0f6231949aca9d9e52de019e87f613
Srijita Mondal <quokkaiscute1409@gmail.com>
2026-02-27 00:53:14 +0530

    Create elgamal.py

---
+++ elgamal.py
@@ -0,0 +1,21 @@
+import random
+
+# small demo parameters (educational)
+p = 30803
+g = 2
+x = 12345  # private key
+y = pow(g, x, p)  # public key
+
+def encrypt_vote(message: str):
+    m = sum(ord(c) for c in message) % p
+    k = random.randint(2, p-2)
+    c1 = pow(g, k, p)
+    c2 = (m * pow(y, k, p)) % p
+    return (c1, c2)
+
+def decrypt_vote(cipher):
+    c1, c2 = cipher
+    s = pow(c1, x, p)
+    s_inv = pow(s, -1, p)
+    m = (c2 * s_inv) % p
+    return m
